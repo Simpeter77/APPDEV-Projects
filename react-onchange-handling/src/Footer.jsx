59693f96557ed772ac660d4adbf0c11d5e6f2f07
gonzaga_j <gonzaga_j@f112.lan>
2024-11-13 11:56:09 +0800

    react-onchange

---
+++ react-onchange-handling/src/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+
+    return (
+        <footer>
+            <hr />
+            <p>&copy; {new Date().getFullYear()} My Website|| Written by: James Peter Gonzaga</p>
+        </footer>
+    )
+}
